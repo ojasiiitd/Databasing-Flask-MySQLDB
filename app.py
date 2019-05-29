94f90ae0ea8a7cf7211816d4b587e3dc41f465a0
ojasiiitd <ojassaxena03@gmail.com>
2019-05-29 20:20:43 +0530

    update

--- app.py
+++ app.py
@@ -1,14 +1,60 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , flash , redirect , url_for , session , logging , request
+from flask_mysqldb import MySQL
+from wtforms import Form , StringField , TextAreaField , validators;
 
 app = Flask(__name__)
+
+# Setting up MariaDB
+app.config["MYSQL_HOST"] = 'localhost'
+app.config["MYSQL_USER"] = 'root'
+app.config["MYSQL_PASSWORD"] = 'ojasva392000'
+app.config["MYSQL_DB"] = 'prac'
+app.config["MYSQL_CURSORCLASS"] = 'DictCursor'
+# Initialising MariaDB
+mysql = MySQL(app)
 
 @app.route('/')
 def homepage():
 	return render_template('information.html')
 
-@app.route('/survey')
+class suggestions(Form) :
+	name = StringField("Name" , validators = [
+		validators.input_required(),
+		validators.Length(min=1 , max=40)
+		])
+	email = StringField("Email-ID" , validators = [
+		validators.input_required(),
+		validators.Length(min=5 , max=50)
+		])
+	sugg = TextAreaField("Your Suggestion" , validators = [
+		validators.input_required(),
+		validators.Length(min=1 , max=500)
+		])
+
+@app.route('/survey' , methods = ["GET" , "POST"])
 def survey():
-	return render_template('survey.html')
+	form = suggestions(request.form)
+	if request.method == "POST" and form.validate() :
+		name = form.name.data
+		email = form.email.data
+		sugg = form.sugg.data
+
+		# Create Cursor
+		cur = mysql.connection.cursor()
+
+		# Execute query
+		cur.execute("insert into prac values(%s , %s , %s)" , (name , email , sugg))
+
+		# Commit to DB
+		mysql.connection.commit()
+		cur.close()
+
+		flash("Your suggestion has been recorded. Thank you for your time." , "success")
+		redirect(url_for("homepage"))
+		return render_template('survey.html' , form = form)
+
+	return render_template('survey.html' , form = form)
 
 if __name__ == '__main__':
-	app.run(port = "4000")
+	app.secret_key = "thisisthebestsecretkeyever"
+	app.run()
