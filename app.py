1bf53e9c5109288ba00efc56384177af1f7df7a8
ojasiiitd <ojassaxena03@gmail.com>
2019-05-30 23:05:58 +0530

    updated secretkey and mysql passwd

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Setting up MariaDB
 app.config["MYSQL_HOST"] = 'localhost'
 app.config["MYSQL_USER"] = 'root'
-app.config["MYSQL_PASSWORD"] = 'ojasva392000'
+app.config["MYSQL_PASSWORD"] = ''
 app.config["MYSQL_DB"] = 'prac'
 app.config["MYSQL_CURSORCLASS"] = 'DictCursor'
 # Initialising MariaDB
